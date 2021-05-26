91d513757fa7a8959b1469174a68959bf5052f90
Peter <griffitp12@gmail.com>
2021-05-26 17:22:29 +0900

    started work on pwa manifest

---
+++ vue.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  devServer: {
+    port: 3000
+  },
+  // THIS IS THE PWA MANIFEST I THINK
+  pwa: {
+    manifestOptions: {
+      name: "OneRoof",
+      short_name: "OneRoof",
+      start_url: "./",
+      display: "standalone",
+      theme_color: "#000000",
+      icons: [
+        {
+          src: "./img/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable"
+        }
+      ]
+    },
+
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    iconPaths: {
+      maskicon: null,
+      favicon32: "./favicon32.png",
+      favicon16: "./favicon16.png",
+      appleTouchIcon: null,
+      msTileImage: null,
+    },
+    // configure the workbox plugin
+    workboxPluginMode: "GenerateSW",
+  },
+};
