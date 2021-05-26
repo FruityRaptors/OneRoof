cc09fa32cb0f55879b8f469bf38c2530221c7f82
Peter <griffitp12@gmail.com>
2021-05-26 11:50:07 +0900

    firebase initialized

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import './registerServiceWorker'
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "YourConfigHere",
+  authDomain: "YourConfigHere",
+  projectId: "YourConfigHere",
+  storageBucket: "YourConfigHere",
+  messagingSenderId: "YourConfigHere",
+  appId: "YourConfigHere"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
