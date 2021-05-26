775752fb156030eb5d7ca5ec859cfd512578a6fb
jaysutham <jaysutham@gmail.com>
2021-05-26 15:12:22 +0900

    test

---
+++ src/views/Register.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form @submit.prevent="register">
+      <h2>Register</h2>
+      <input type="email" placeholder="Email address..." v-model="email" />
+      <input type="password" placeholder="password..." v-model="password" />
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+  name: "Register",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+  register() {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.email, this.password)
+      .then(() => {
+        alert('Successfully registered! Please login.');
+        this.$router.push('/yourhome');
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  },
+},
+};
+</script>
+
+<style>
+</style>
