01a68a8213a190f27b410dfefe05eb19f8004f45
Evan Trujillo <evomatic@pop-os.localdomain>
2021-05-27 22:24:44 +0900

    modified ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
    "type": "postgres",
-   "host": `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}` || "127.0.0.1",
+   "host": process.env.DB_INSTANCE || "127.0.0.1",
    "port": process.env.DB_PORT,
    "username": process.env.DB_USER,
    "password": process.env.DB_PASS,
    "database": process.env.DB_NAME,
-   "socketPath": `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+   "socketPath": process.env.DB_INSTANCE,
    "synchronize": true,
    "logging": false,
    "entities": [
