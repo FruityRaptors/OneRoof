d558b816a401c174ca05d1dcea393c322fc669f0
Peter <griffitp12@gmail.com>
2021-05-27 15:55:13 +0900

    updated registration to include nickname

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,28 +2,50 @@
   <div>
     <form @submit.prevent="register">
       <h2>Register</h2>
-      <input type="email" placeholder="Email address..." v-model="email" />
-      <input type="password" placeholder="password..." v-model="password" />
+      <section>
+        <label for="name">What do your housemates call you?</label>
+        <input id="name" type="text" placeholder="name..." v-model="username" />
+      </section>
+      <section>
+        <label for="email">Please enter a valid email address</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Email address..."
+          v-model="email"
+        />
+      </section>
+      <section>
+        <label for="password">Enter a password, at least 6 characters</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="password..."
+          v-model="password"
+        />
+      </section>
+
       <button type="submit">Register</button>
     </form>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Register",
   data() {
     return {
       email: "",
       password: "",
+      username: "",
     };
   },
   methods: {
     register() {
       let email = this.email;
       let password = this.password;
-      this.$store.dispatch("registerUser", { email, password });
+      let username = this.username;
+      this.$store.dispatch("registerUser", { email, password, username });
     },
   },
 };
