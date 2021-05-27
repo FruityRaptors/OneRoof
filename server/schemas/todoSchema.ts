3996479d17259aad2d0177b9e4bec4ac6dd3e82d
jaysutham <jaysutham@gmail.com>
2021-05-27 11:39:30 +0900

    gqladded

---
+++ server/schemas/todoSchema.ts
@@ -0,0 +1,25 @@
+import { Field, ObjectType, InputType } from 'type-graphql';
+
+@ObjectType()
+    export class Todo {
+        @Field()
+        id: number
+
+        @Field()
+        todo: string
+
+        @Field()
+        category: number
+
+        @Field()
+        house_key: string
+
+        @Field()
+        date: string
+
+        @Field()
+        victimid: [string]
+
+        @Field()
+        creatorid: string
+    }
