12e5583c42c33a9c10751547f6e2576f08abc1dd
Jay <jaysutham@gmail.com>
2021-05-28 10:24:01 +0900

    added organization

--- src/views/joinHouse.vue
+++ src/views/joinHouse.vue
@@ -1,9 +1,14 @@
 <template>
 <div>
+
+<!-- Main card starts -->
  <v-card class="mx-auto text-center mt-10" max-width="400px" rounded>
+
      <v-card-title class="text-center">Looks like you don't have a home yet...</v-card-title>
+
          <v-card-text class="font-weight-bold">Enter House key</v-card-text>
    
+<!-- Join a home field starts -->
         <v-text-field
             label="House Key..."
             class="ml-5 mr-5"
@@ -12,7 +17,11 @@
             rounded
             v-model="roomkey"
           ></v-text-field>  
+<!-- Join a home field ends -->
+
      <v-card-subtitle class="font-weight-bold">or..</v-card-subtitle>
+
+<!-- Name your home field starts-->
      <v-text-field
             label="Name your Home"
             class="ml-5 mr-5"
@@ -21,16 +30,22 @@
             rounded
             v-model="homename"
           ></v-text-field>  
+<!-- Name your home field ends -->
+    
+
      <v-btn
       rounded
       color="brown"
       dark
       class="mb-5"
       @click="joinHouse"
-    >
+      >
       Make a Home
     </v-btn>
+
  </v-card>
+<!-- Main card ends -->
+
  </div>
 </template>
 
