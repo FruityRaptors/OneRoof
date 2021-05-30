e3bb756a47ee4d7bfb89021722d58ffef9c2d29b
Evan Trujillo <evomatic@pop-os.localdomain>
2021-05-30 15:10:51 +0900

    gcp prep again

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
    "type": "postgres",
-   "host": process.env.CLOUD_SQL_CONNECTION_NAME || "127.0.0.1",
+   "host": process.env.DB_INSTANCE || "127.0.0.1",
    "port": process.env.DB_PORT,
    "username": process.env.DB_USER,
    "password": process.env.DB_PASS,
    "database": process.env.DB_NAME,
-   "socketPath": process.env.CLOUD_SQL_CONNECTION_NAME,
+   "socketPath": process.env.DB_INSTANCE,
    "synchronize": true,
    "logging": false,
    "entities": [
@@ -23,3 +23,5 @@ module.exports = {
       "subscribersDir": "server/subscriber"
    }
 }
+
+//"concurrently \"npm run serve\" \
