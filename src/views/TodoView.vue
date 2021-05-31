a4b95667f93cda397ac9804b4e84b244c167b532
Ken Kurosaki <ken.kurosaki1991@gmail.com>
2021-05-31 17:28:32 +0900

    Trying to pull

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -13,7 +13,7 @@
             clearable
             hide-details
           ></v-text-field>
-      <v-list class="pt-0" two-line flat>
+      <v-list v-if="todos.length" class="pt-0" two-line flat>
         <div id="todo-container" v-for="todo in todos" :key="todo.id">
           <v-list-item @click="completeTodo(todo.id)" :class="{ 'green lighten-4' : todo.complete }">
             <template v-slot:default>
@@ -28,7 +28,11 @@
                 <v-list-item-subtitle>{{ todo.victimid }}</v-list-item-subtitle>
               </v-list-item-content>
               <v-list-item-action>
-          <v-btn icon @click.stop="deleteTodo(todo.id)">
+          <v-btn icon>
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+          <v-btn icon @click.stop="modals.deleteTodo = true"> 
+            <!-- @click.stop="deleteTodo(todo.id)" -->
             <v-icon color="brown lighten-3">mdi-delete-sweep-outline</v-icon>
           </v-btn>
         </v-list-item-action>
@@ -37,19 +41,32 @@
           <v-divider></v-divider>
         </div>
       </v-list>
+      <div v-else>
+        <div id="no-todo-bg">
+          NO TODOS
+        </div>
+      </div>
     </div>
+    <DeleteModal v-if="modals.deleteTodo" v-bind:todo='todo' />
   </div>
 </template>
 
 <script>
 // import TodoList from '../components/TodoList.vue'
+import DeleteModal from './Modals/DeleteModal.vue'
 
 export default {
   name: "Home",
+  components: {
+    DeleteModal
+  },
   data() {
     return {
       newTodoMessage: 'HERE I CAM',
       todos: [],
+      modals: {
+        deleteTodo: false,
+      },
     };
   },
  async mounted() {
@@ -62,7 +79,7 @@ export default {
         let newTodo = {
             todo: this.newTodoMessage,
             date: Date.now(),
-            victimid: "maybe in a future update",
+            victimid: "everyone",
             creatorid: this.$store.state.user.username,
             complete: false,
         }
