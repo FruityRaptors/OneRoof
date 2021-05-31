7d9b8945410a37fe8ade3e990129ecddc7a0a6d9
Ken Kurosaki <ken.kurosaki1991@gmail.com>
2021-05-31 17:28:32 +0900

    Trying to pull

---
+++ src/views/Modals/DeleteModal.vue
@@ -0,0 +1,39 @@
+ <template>
+ <v-dialog
+      persistent
+      max-width="290"
+      :value = "true"
+    >   
+      <v-card>
+        <v-card-title class="headline">
+          Delete Todo
+        </v-card-title>
+        <v-card-text>You sure you wanna do that you Dingus?</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green lighten-0"
+            text
+            @click="dialog = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            @click="$store.dispatch('deleteTodo', todo.id)"
+            color="green lighten-0"
+            text
+          >
+            DELETE
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+ </template>
+
+ <script>
+ export default {
+    name: 'Test',
+    props: ['todo']
+};
+ </script>
+ 
