6e63379354f38e6a6029ef142c93e99ad94ce004
Peter <griffitp12@gmail.com>
2021-06-04 13:26:39 +0900

    tried to make chores more reactive, failed

--- src/views/ChoreView.vue
+++ src/views/ChoreView.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="chore-page orange lighten-5" id="chore-page-container">
-    <AddChore />
+  <div
+    class="chore-page orange lighten-5"
+    id="chore-page-container"
+  >
     <!-- chore list platform -->
-    <v-list v-if="chores.length" class="pa-0 orange lighten-5" two-line flat>
+    <v-list class="pa-0 orange lighten-5" two-line flat>
       <!-- Each Chore in Chore list -->
       <div
         id="chore-container"
@@ -21,64 +23,29 @@
             </v-list-item-subtitle>
           </v-list-item-content>
           <!-- Todo list text ends -->
-
-          <!-- Delete Button -->
-          <v-list-item-action class="">
-            <v-icon color="orange accent-3"> mdi-delete</v-icon>
-
-            <!-- Delete Button ends-->
-            <!-- Assignee Dropdown Starts -->
-            <v-menu offset-y>
-              <template v-slot:activator="{ on, attrs }">
-                <v-icon
-                  class="pt-2 pr-1"
-                  color="orange accent-3"
-                  v-bind="attrs"
-                  v-on="on"
-                  >mdi-account-plus</v-icon
-                >
-              </template>
-              <v-list>
-                <v-list-item
-                  v-for="user in users"
-                  :key="user.id"
-                  :value="chore"
-                >
-                  <v-list-item-title @click="setAssignee(user, chore)">
-                    {{ user.username }}
-                  </v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </v-list-item-action>
-          <!-- Assignee Dropdown Ends -->
         </v-list-item>
         <!-- Border Between Todos -->
         <v-divider :key="chore.id"></v-divider>
       </div>
+      <AddChore @addThisChorePlease="pushChore" />
+      <button @click="resetChores">reset chores (dev)</button>
       <!-- Each Todo in Todo list ends-->
     </v-list>
     <!-- todo list platform ends-->
-
-    <!-- No Todos if todo list is empty -->
-    <div v-else class="no-todo-bg">
-      <v-icon size="100" color="green darken-2" id="icon-chore"
-        >mdi-help-box</v-icon
-      >
-      <div class="text-h5">Add a Chore!</div>
-    </div>
-    <!-- Delete Modal -->
   </div>
 </template>
 
 <script>
-// import TodoList from '../components/TodoList.vue'
+import AddChore from "../components/AddChore.vue";
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    AddChore,
+  },
   data() {
     return {
+      rerenderkey: 0,
       newChoreName: "",
       chores: [],
       modals: {
@@ -90,16 +57,21 @@ export default {
     };
   },
   mounted() {
-    // these lines add a seed chore; they can be deleted after we're live with this module
-    let seedChore = {
-      id: 1,
-      chore: "Mow the lawn",
-      frequency: "weekly",
-    };
-    this.$store.commit("resetChorelist")
-    this.$store.commit("addChore", seedChore);
-    // seed chores end
+    console.log("mounting");
     this.chores = this.$store.state.chores;
+  },
+  methods: {
+    pushChore(chore) {
+      console.log("Pushing chore:", chore);
+      this.$store.commit("addChore", chore);
+    },
+
+    //for dev purposes only
+    resetChores() {
+      this.$store.commit("resetChorelist");
+    },
+
+    //end dev methods
   },
 };
 </script>
