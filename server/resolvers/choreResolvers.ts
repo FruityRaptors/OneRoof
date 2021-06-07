234336763fa703b65697ab6aabdf66706eb20f99
Peter <griffitp12@gmail.com>
2021-06-07 15:45:39 +0900

    Chores can be added and gotten!

--- server/resolvers/choreResolvers.ts
+++ server/resolvers/choreResolvers.ts
@@ -4,7 +4,27 @@ import { Chores } from '../entity/Chores'
 @Resolver()
 export class choreResolvers {
     @Query(() => [Chores])   
-    getAllTodos(){
+    getAllChores(){
         return Chores.find()
     }
+
+    @Query(() => [Chores])
+    getChoresByHouse(
+        @Arg('house_key') house_key: string,
+    ){
+        return Chores.find( { where: { house_key } } )
+    }
+
+    @Mutation(() => String)
+    createChore(
+        @Arg('chore') chore: string,             
+        @Arg('description') description: string,        
+        @Arg('asignee') asignee: string,     
+        @Arg('creatorid') creatorid: string,   
+        @Arg('house_key') house_key: string,   
+        ){
+
+        Chores.insert({ chore, house_key, description, asignee, creatorid,})
+        return "New chore Added!"
+      }
 }
