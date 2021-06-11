cdc03a04af94a7d38c13277f51f272c874ad74fc
Peter <griffitp12@gmail.com>
2021-06-11 16:38:44 +0900

    Set up for photo_url to appear on chores

--- src/views/Modals/AssignChore.vue
+++ src/views/Modals/AssignChore.vue
@@ -109,11 +109,16 @@ export default {
       this.$emit("closeAssignModalPlease");
     },
     assignChoreAndClose() {
-      let newAssignee = "";
+      let newAssignee= {}
       if (this.newAssignee) {
-        newAssignee = this.newAssignee;
+        newAssignee.username = this.newAssignee;
       } else if (this.newAssigneeRandom) {
-        newAssignee = this.newAssigneeRandom;
+        newAssignee.username = this.newAssigneeRandom;
+      }
+      for (let user of this.users) {
+        if (user.username === newAssignee.username) {
+          newAssignee.photo_url = user.photo_url
+        }
       }
       this.$emit("assignChorePlease", newAssignee);
     },
