19264ac5cd9cedbcd075eeb6d9be5a588d89848c
Peter <griffitp12@gmail.com>
2021-07-06 17:16:33 +0900

    uncommented dev server setup

--- server/index.ts
+++ server/index.ts
@@ -25,15 +25,14 @@ import sqlConfig from '../ormconfig'
   
   const app = express();
 
-/*   app.use(express.static(path.resolve(__dirname,"..", "..", "dist")))
+  app.use(express.static(path.resolve(__dirname,"..", "..", "dist")))
 
   app.use(history({
     disableDotRule: true,
     verbose: true
   }))
 
-
-  app.use(express.static(path.resolve(__dirname,"..", "..", "dist"))) */
+  app.use(express.static(path.resolve(__dirname,"..", "..", "dist")))
 
   app.set('view engine', 'pug');
 
